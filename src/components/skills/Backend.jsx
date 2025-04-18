6188740a96cea87655003f697772a8aa4434256d
kirbybucket <haroldmoncadasoria@gmail.com>
2025-04-17 20:42:12 -0400

    feat(skills): update backend icons with technology-specific visuals
    - Replace generic check icons with Java/C++/Python brand logos
    - Add dedicated icons for Bash, Firebase and SQL

--- src/components/skills/Backend.jsx
+++ src/components/skills/Backend.jsx
@@ -8,7 +8,7 @@ const Backend = () => {
             <div className="skills__box">
                 <div className="skills__group">
                     <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
+                        <i class='bx bxl-java'></i>
 
                         <div>
                             <h3 className="skills__name">Java</h3>
@@ -17,7 +17,7 @@ const Backend = () => {
                     </div>
 
                     <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
+                        <i class='bx bxl-c-plus-plus' ></i>
 
                         <div>
                             <h3 className="skills__name">C++</h3>
@@ -26,7 +26,7 @@ const Backend = () => {
                     </div>
 
                     <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
+                        <i class='bx bxl-python' ></i>
 
                         <div>
                             <h3 className="skills__name">Python</h3>
@@ -36,33 +36,33 @@ const Backend = () => {
                 </div>
 
                 <div className="skills__group">
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
+                    <div className="skills__data">
+                        <i class='bx bxs-terminal'></i>
 
-                    <div>
-                        <h3 className="skills__name">Bash</h3>
-                        <span className="skills__level">Basic</span>
+                        <div>
+                            <h3 className="skills__name">Bash</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i class='bx bxl-firebase' ></i>
+
+                        <div>
+                            <h3 className="skills__name">Firebase</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
+                    </div>
+
+                    <div className="skills__data">
+                        <i class='bx bxs-data' ></i>
+
+                        <div>
+                            <h3 className="skills__name">SQL</h3>
+                            <span className="skills__level">Basic</span>
+                        </div>
                     </div>
                 </div>
-
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-
-                    <div>
-                        <h3 className="skills__name">Firebase</h3>
-                        <span className="skills__level">Basic</span>
-                    </div>
-                </div>
-
-                <div className="skills__data">
-                    <i class='bx bx-badge-check'></i>
-
-                    <div>
-                        <h3 className="skills__name">SQL</h3>
-                        <span className="skills__level">Basic</span>
-                    </div>
-                </div>
-            </div>
             </div>
         </div>
     )
